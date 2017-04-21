7463d1cb5aefca5453bae0bbeb9cd359e2920bd4
sgiehl <stefan@piwik.org>
2017-04-21 21:13:45 +0200

    use selectframe

--- tests/UI/specs/Overlay_spec.js
+++ tests/UI/specs/Overlay_spec.js
@@ -70,15 +70,11 @@ describe("Overlay", function () {
 
     it("should show stats for new links when dropdown opened", function (done) {
         expect.screenshot("page_new_links").to.be.capture(function (page) {
-            var pos = page.webpage.evaluate(function () {
-                var iframe = $('iframe'),
-                    innerOffset = $('.dropdown-toggle', iframe.contents()).offset();
-                return {
-                    x: iframe.offset().left + innerOffset.left + 32, // position is incorrect for some reason w/o adding pixels
-                    y: iframe.offset().top + innerOffset.top
-                };
+            page.evaluate(function() {
+                var iframe = document.getElementById('overlayIframe');
+                var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+                innerDoc.querySelector('.dropdown').click();
             });
-            page.sendMouseEvent('click', pos);
             page.wait(2000);
 
             removeOptOutIframe(page);
